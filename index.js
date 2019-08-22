5900a2da0b9dd3287a794bd900965b6ef781eff5
André Tronca <andretronca@gmail.com>
2019-08-21 21:27:58 -0300

    route: create project

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ const server = express();
 
 server.use(express.json());
 
+const projects = [];
+
+// Create Project
+server.post("/projects", (req, res) => {
+  const project = req.body;
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
 server.listen(3000);
